97e6d34d5585b5a7b52b509880f342d3196bac43
Joanna Fiołka <joanafiolka@gmail.com>
2023-04-06 13:28:10 +0200

    Connected to the database

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
 
+
+dotenv.config()
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => console.log("DB Connection Successfull!"))
+    .catch((err) => {
+        console.log(err);
+    })
+
+dotenv.config()
 app.use(express.json());
 app.use(cookieParser());
+
 app.listen(process.env.PORT || 3002, '0.0.0.0', () => {
     console.log('Connected to backend http://localhost:3002')
 })
