e79f50ff5e3b49a10e5212f4c16525c9ca84be7d
Joanna Fiołka <joanafiolka@gmail.com>
2023-04-06 13:29:50 +0200

    Added rate-limiter.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+
 const app = express();
 
 
@@ -16,6 +19,13 @@ mongoose
 dotenv.config()
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({origin: 'http://localhost:3000',}));
+
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 100,
+}));
 
 app.listen(process.env.PORT || 3002, '0.0.0.0', () => {
     console.log('Connected to backend http://localhost:3002')
